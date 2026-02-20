9ce41d3bd0c7d3c06d9bb2d5ae3e48b4e9e2c4ff
saurav <sauravthespi@gmail.com>
2026-02-20 20:40:21 +0530

    yoyo

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// gsap.to("#box",{
-//     x:1000,
-//     duration:2,
-//     delay:1
+gsap.to("#box",{
+    x:1000,
+    duration:1,
+    delay:1,
+    rotate:360,
+    repeat:-1,
+    yoyo:true
+
     
-// })
+})
 // gsap.to ka matlab hai inital se final position ko ------->
 // gsap.from me final to initial jata hai <------------
 // gsap.from("#box2",{
@@ -15,14 +19,22 @@
 // })
 
 
-gsap.to("#box",{
-    x:1200,
-    duration:2,
-    delay:1,
-    rotate:360,
-    backgroundColor:"blue",
-    borderRadius:"50%",
-    scale:0.2
+// gsap.to("#box",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+//     scale:0.2
 
 
-})
+// })
+// gsap.from("h1",{
+//     // color:"red",
+//     opacity:0,
+//     duration:1,
+//     delay:1,
+//     y:20,
+//     stagger:0.3
+// })
