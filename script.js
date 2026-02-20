2d14e7b2b4d1fdb486a7fa843a6863945ff7731b
saurav <sauravthespi@gmail.com>
2026-02-20 20:29:57 +0530

    rotator

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-gsap.to("#box",{
-    x:1000,
-    duration:2,
-    delay:1
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1
     
-})
+// })
 // gsap.to ka matlab hai inital se final position ko ------->
 // gsap.from me final to initial jata hai <------------
-gsap.from("#box2",{
-    x:1000,
-    y:500,
+// gsap.from("#box2",{
+//     x:1000,
+//     y:500,
+//     duration:2,
+//     delay:2
+
+// })
+
+
+gsap.to("#box",{
+    x:1200,
     duration:2,
-    delay:2
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.2
+
 
 })
