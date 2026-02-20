417789c2e5b5bb04df14dfa675ab6a8602cf5660
saurav <sauravthespi@gmail.com>
2026-02-20 20:55:37 +0530

    timeline

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-gsap.to("#box",{
-    x:1000,
-    duration:1,
-    delay:1,
-    rotate:360,
-    repeat:-1,
-    yoyo:true
+// gsap.to("#box",{
+//     // x:1000,
+//     // duration:1,
+//     // delay:1,
+//     // rotate:360,
+//     // repeat:-1,
+//     // yoyo:true
+//     x:1200,
+//     rotate:360,
+//     delay:1,
+//     duration:1.5,
+//     repeat:-1,
+//     yoyo:true
+
 
     
-})
-// gsap.to ka matlab hai inital se final position ko ------->
-// gsap.from me final to initial jata hai <------------
-// gsap.from("#box2",{
-//     x:1000,
-//     y:500,
-//     duration:2,
-//     delay:2
+// })
+// gsap.to("#box2",{
+//     x:1200,
+//     rotate:360,
+//     duration:1.5,
+//     delay:2.5,
+//     repeat:-1,
+//     yoyo:true
 
 // })
+// gsap.to("#box3",{
+//     // x:1000,
+//     // duration:1,
+//     // delay:1,
+//     // rotate:360,
+//     // repeat:-1,
+//     // yoyo:true
+//     x:1200,
+//     rotate:360,
+//     delay:4,
+//     duration:1.5,
+//     repeat:-1,
+//     yoyo:true,
+//     backgroundColor:"yellow",
+//     borderRadius:"50%"
+
+
+    
+// })
+// gsap.to ka matlab hai inital se final position ko ------->
+// gsap.from me final to initial jata hai <------------
 
 
 // gsap.to("#box",{
@@ -38,3 +66,28 @@ gsap.to("#box",{
 //     y:20,
 //     stagger:0.3
 // })
+
+// var tl = gsap.timeline();
+// tl.to("#box",{
+//     x:1200,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1
+// })
+// tl.to("#box2",{
+//     x:1200,
+//     rotate:360,
+//     duration:1.5,
+//     borderRadius:"50%"
+    
+// })
+// tl.to("#box3",{
+//     x:1200,
+//     rotate:360,
+//     duration:1.5,
+    
+    
+// })
+
+
+
